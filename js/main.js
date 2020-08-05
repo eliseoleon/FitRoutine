d1c33d03dad1d184761612f95ea462bba66ef630
eliseoleon <eliseoleonpaez@gmail.com>
2020-08-04 22:53:54 -0500

    Primer dashboard

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+//Ocultar elemenmto
+$('.up').hide();
+
+$(window).scroll(function () { 
+    if ($(this).scrollTop() > 100 ) {
+        $('.up').fadeIn('500');
+    }else{
+        $('.up').fadeOut('500');
+    }
+});
+
+$('.up').click(function () {
+    $('body, html').animate({
+        scrollTop: 0,
+
+    },300);
+})
+
+});
